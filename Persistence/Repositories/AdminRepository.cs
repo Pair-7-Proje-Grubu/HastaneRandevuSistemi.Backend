d9c4c08ab37b5766afb7d79c4235830aa0d69363
streamworlddev <streamworld15@gmail.com>
2024-06-03 15:01:50 +0300

    add concrete repositories

---
+++ Persistence/Repositories/AdminRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.DataAccess;
+using Domain.Entities;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repositories
+{
+    public class AdminRepository : EfRepositoryBase<Admin, HRSDbContext>, IAdminRepository
+    {
+        public AdminRepository(HRSDbContext context) : base(context)
+        {
+        }
+    }
+
+}
