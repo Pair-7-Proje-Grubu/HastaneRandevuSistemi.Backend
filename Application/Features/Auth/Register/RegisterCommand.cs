ea80b8961fe217f0cd563367c171cbeed53d69e7
eraygok <eraygok2001@hotmail.com>
2024-06-03 16:11:29 +0300

    add Auth features

---
+++ Application/Features/Auth/Register/RegisterCommand.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auth.Register
+{
+    public class RegisterCommand : IRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
+        public char Gender { get; set; }
+
+        public class RegisterCommandHandler : IRequestHandler<RegisterCommand>
+        {
+            private readonly IRequestHandler<RegisterCommand> _handler;
+            public Task Handle(RegisterCommand request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        
+
+    }
+
+    
+}
