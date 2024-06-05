6e15ba90fc709d575e053fca67e5baedd0136eb7
Ahmet Yüksel <ahmetyuksel.ms@outlook.com>
2024-06-05 15:33:59 +0300

    "NoWorkHour" and "Report" delete command validation operations.

---
+++ Application/Features/NoWorkHours/Commands/Delete/DeleteNoWorkHourCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.NoWorkHours.Commands.Delete
+{
+    public class DeleteNoWorkHourCommandValidator : AbstractValidator<DeleteNoWorkHourCommand>
+    {
+        public DeleteNoWorkHourCommandValidator()
+        {
+            RuleFor(p => p.Id)
+                .NotEmpty().WithMessage("Id is required.")
+                .NotNull();
+        }
+    }
+}
