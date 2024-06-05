af9a36028e0be5e74513877679bb429bd16c718c
streamworlddev <streamworld15@gmail.com>
2024-06-05 14:05:21 +0300

    add cqrs UpdateDoctor

---
+++ Application/Features/Doctors/Commands/UpdateDoctor/UpdateDoctorCommand.cs
@@ -0,0 +1,53 @@
+﻿using Application.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Doctors.Commands.UpdateDoctor
+{
+    public class UpdateDoctorCommand :  IRequest<UpdateDoctorResponse>
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        //public string Password { get; set; }
+        public DateTime BirthDate { get; set; }
+        public char Gender { get; set; }
+        public int TitleId { get; set; }
+        public int ClinicId { get; set; }
+        public int OfficeLocationId { get; set; }
+
+
+        public class UpdateDoctorCommandHandler : IRequestHandler<UpdateDoctorCommand, UpdateDoctorResponse>
+        {
+
+            private readonly IDoctorRepository _doctorRepository;
+            private readonly IMapper _mapper;
+
+            public UpdateDoctorCommandHandler(IDoctorRepository doctorRepository, IMapper mapper)
+            {
+                _doctorRepository = doctorRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<UpdateDoctorResponse> Handle(UpdateDoctorCommand request, CancellationToken cancellationToken)
+            {
+                Doctor doctor = _mapper.Map<Doctor>(request);
+
+                await _doctorRepository.UpdateAsync(doctor);
+
+                UpdateDoctorResponse response = new UpdateDoctorResponse();
+                return response;
+            }
+        }
+
+
+    }
+}
