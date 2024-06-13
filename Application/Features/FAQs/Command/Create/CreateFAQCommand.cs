8f1a24f85c00b33d6a10a6f22f9eb888af7db73d
eraygok <eraygok2001@hotmail.com>
2024-06-13 20:10:23 +0300

    add faq cqrs

---
+++ Application/Features/FAQs/Command/Create/CreateFAQCommand.cs
@@ -0,0 +1,40 @@
+﻿using Application.Features.Blocks.Commands.Create;
+using Application.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.FAQs.Command.Create
+{
+    public class CreateFAQCommand : IRequest<CreateFAQResponse>
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public class CreateFAQCommandHandler : IRequestHandler<CreateFAQCommand, CreateFAQResponse>
+        {
+            private readonly IFAQRepository _faqRepository;
+            private readonly IMapper _mapper;
+            public CreateFAQCommandHandler(IFAQRepository faqRepository, IMapper mapper)
+            {
+                _faqRepository = faqRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<CreateFAQResponse> Handle(CreateFAQCommand request, CancellationToken cancellationToken)
+            {
+                FAQ faq = _mapper.Map<FAQ>(request);
+                await _faqRepository.AddAsync(faq);
+
+                CreateFAQResponse response = _mapper.Map<CreateFAQResponse>(faq);
+                return response;
+            }
+        }
+
+    }
+}
