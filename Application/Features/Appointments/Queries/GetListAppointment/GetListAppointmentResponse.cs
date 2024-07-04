cbbc7cf1df11683dbcb2abd29e83b75b63c9021e
streamworlddev <streamworld15@gmail.com>
2024-07-04 21:39:47 +0300

    refactor: appointment

--- Application/Features/Appointments/Queries/GetListAppointment/GetListAppointmentResponse.cs
+++ Application/Features/Appointments/Queries/GetListAppointment/GetListAppointmentResponse.cs
@@ -1,10 +1,14 @@
-﻿namespace Application.Features.Appointments.Queries.GetListAppointment
+﻿using Domain.Entities;
+
+namespace Application.Features.Appointments.Queries.GetListAppointment
 {
     public class GetListAppointmentResponse
     {
         public DateTime DateTime { get; set; }
         public string OfficeLocation { get; set; }
-        public string DoctorName { get; set; }
-        public string ClinicName { get; set; }
+        public string Doctor { get; set; }
+        public string Clinic { get; set; }
+        public CancelStatus CancelStatus { get; set; }
+
     }
 }
