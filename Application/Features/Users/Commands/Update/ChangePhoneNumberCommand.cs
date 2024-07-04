b5d4d9aa43b3033bce78768c610f0f115035c8b2
Mertcan Sezginer <mertcansezginer@hotmail.com>
2024-07-04 13:49:19 +0300

    feat:  change phone and password

---
+++ Application/Features/Users/Commands/Update/ChangePhoneNumberCommand.cs
@@ -0,0 +1,39 @@
+﻿using Application.Repositories;
+using AutoMapper;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Users.Commands.Update
+{
+    public class ChangePhoneNumberCommand : IRequest<ChangePhoneNumberResponse>
+    {
+        public string Email { get; set; }
+        public string NewPhone { get; set; }
+
+        public class ChangePhoneNumberCommandHandler : IRequestHandler<ChangePhoneNumberCommand, ChangePhoneNumberResponse>
+        {
+            private readonly IPatientRepository _patientRepository;
+            private readonly IMapper _mapper;
+
+            public ChangePhoneNumberCommandHandler(IPatientRepository patientRepository, IMapper mapper)
+            {
+                _patientRepository = patientRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<ChangePhoneNumberResponse> Handle(ChangePhoneNumberCommand request, CancellationToken cancellationToken)
+            {
+                Patient? patient = await _patientRepository.GetAsync(p => p.Email == request.Email);
+                if (patient == null)
+                    throw new BusinessException("Hasta bulunamadı");
+
+                patient.Phone = request.NewPhone;
+                await _patientRepository.UpdateAsync(patient);
+
+                ChangePhoneNumberResponse response = _mapper.Map<ChangePhoneNumberResponse>(patient);
+                return response;
+            }
+        }
+    }
+}
---
+++ Application/Features/Users/Profiles/UserMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Application.Features.Users.Commands.Update;
+
+namespace Application.Features.Users.Profiles
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<Patient, ChangePhoneNumberResponse>()
+                .ForMember(dest => dest.IsSuccess, opt => opt.MapFrom(src => true))
+                .ForMember(dest => dest.Message, opt => opt.MapFrom(src => "Telefon numarası başarıyla güncellendi"));
+        }
+    }
+}
