d131c535a8cda7a2dd097338d44d9dd3295d8b93
streamworlddev <streamworld15@gmail.com>
2024-07-12 13:45:48 +0300

    feat: implementation business rules

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -1,5 +1,7 @@
-﻿using Core.Application.Pipelines.Authorization;
+﻿using Application.Features.Appointments.Rules;
+using Core.Application.Pipelines.Authorization;
 using Core.Application.Pipelines.Validation;
+using Core.Application.Rules;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,21 +15,39 @@ namespace Application
 {
     public static class ApplicationServiceRegistration
     {
-       public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddMediatR(configuration =>
             {
-                configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());     
+                configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
                 configuration.AddOpenBehavior(typeof(AuthorizationBehavior<,>));
                 configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
 
             });
+
+            services.AddSubClassesOfType(Assembly.GetExecutingAssembly(), typeof(BaseBusinessRules));
 
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
             return services;
         }
-        
+
+        public static IServiceCollection AddSubClassesOfType(
+       this IServiceCollection services,
+       Assembly assembly,
+       Type type,
+       Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null
+   )
+        {
+            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+            foreach (var item in types)
+                if (addWithLifeCycle == null)
+                    services.AddScoped(item);
+                else
+                    addWithLifeCycle(services, type);
+            return services;
+        }
+
     }
 }
