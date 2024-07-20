8acc1069d856a91b8c8e25f15a66ecb52915b2ad
streamworlddev <streamworld15@gmail.com>
2024-07-20 15:01:45 +0300

    feat: constants (appointment, clinic, doctor, workingtime)

---
+++ Application/Features/Appointments/Constants/AppointmentsMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Features.Appointments.Constants
+{
+    public static class AppointmentsMessages
+    {
+        public const string AppointmentHasAlreadyBeenBooked = "Randevu önceden alınmış.";
+        public const string AppointmentTimeIsInvalid = "Randevu zamanı geçersiz.";
+        public const string AppointmentTimeIsOutsideWorkingHours = "Randevu zamanı mesai saatleri dışında.";
+        public const string AppointmentTimeIsDuringBreakHours = "Randevu zamanı mola saatleri içerisinde.";
+        public static string AppointmentTimeIsDuringNoWorkHours(string title) => $"Randevu zamanı doktorun '{title}' nedeniyle müsait olmadığı saatler içerisinde.";
+    }
+
+}
